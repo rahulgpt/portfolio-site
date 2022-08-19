82c0d3d81b4be1f25e2e47bb8ab6f2cb7e4e2348
Rahul Gupta <rahulr09gupta@gmail.com>
2022-08-19 23:13:49 +0530

    feat: add mdx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   experimental: {
     images: {
       allowFutureImage: true,
@@ -8,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
